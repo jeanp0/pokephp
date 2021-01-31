493850ee5ee192e9775d23ee79d48a56e988a07d
jeanp0 <jeanpi3rm@gmail.com>
2021-01-30 20:41:58 -0500

    recognize request method backend

--- backend/all-pokes.php
+++ backend/all-pokes.php
@@ -1,6 +1,6 @@
 <?php
-
-$_POST = json_decode(file_get_contents("php://input"), true);
+$json = file_get_contents("php://input");
+$_POST = json_decode($json, true);
 
 function permissions()
 {
@@ -21,28 +21,30 @@ function permissions()
 permissions();
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header("Content-Type: application/json");
+    if (isset($_POST["name"])) {
+        $api_raw = file_get_contents("https://pokeapi.co/api/v2/pokedex/2/");
+        $result = json_decode($api_raw, true);
 
-if (isset($_POST["name"])) {
-    $api_raw = file_get_contents("https://pokeapi.co/api/v2/pokedex/2/");
-    $result = json_decode($api_raw, true);
+        $pokemons = [];
+        foreach ($result["pokemon_entries"] as $key => $value) {
+            // creo un pokemon (objeto) con los datos obtenidos
+            $id = $value['entry_number'];
+            $name = $value['pokemon_species']['name'];
 
-    $pokemons = [];
-    foreach ($result["pokemon_entries"] as $key => $value) {
-        // creo un pokemon (objeto) con los datos obtenidos
-        $id = $value['entry_number'];
-        $name = $value['pokemon_species']['name'];
+            $pokemon = new stdClass();
+            $pokemon->id = $id;
+            $pokemon->name = ucfirst($name);
 
-        $pokemon = new stdClass();
-        $pokemon->id = $id;
-        $pokemon->name = ucfirst($name);
+            array_push($pokemons, $pokemon);
+        }
 
-        array_push($pokemons, $pokemon);
+        if ($_POST["name"] !== "") {
+            $pokemons = array_values(array_filter($pokemons, function ($poke) {
+                return strpos(strtolower($poke->name), strtolower($_POST["name"])) === 0;
+            }));
+        }
+        echo json_encode($pokemons);
     }
-
-    if ($_POST["name"] !== "") {
-        $pokemons = array_values(array_filter($pokemons, function ($poke) {
-            return strpos(strtolower($poke->name), strtolower($_POST["name"])) === 0;
-        }));
-    }
-    echo json_encode($pokemons);
 }
--- backend/specific-poke.php
+++ backend/specific-poke.php
@@ -1,6 +1,6 @@
 <?php
-
-$_POST = json_decode(file_get_contents("php://input"), true);
+$json = file_get_contents("php://input");
+$_POST = json_decode($json, true);
 
 function permissions()
 {
@@ -20,51 +20,54 @@ function permissions()
 }
 permissions();
 
-if (isset($_POST["id"])) {
-    $pokemon = new stdClass();
-    $id = $_POST["id"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header("Content-Type: application/json");
+    if (isset($_POST["id"])) {
+        $pokemon = new stdClass();
+        $id = $_POST["id"];
 
-    // obtener habilidades y nombre
-    $api_raw = file_get_contents("https://pokeapi.co/api/v2/pokemon/$id");
-    $result = json_decode($api_raw, true);
-    $abilities = array_map(
-        function ($ability) {
-            return $ability["ability"]["name"];
-        },
-        $result["abilities"]
-    );
-    $name = $result["name"];
+        // obtener habilidades y nombre
+        $api_raw = file_get_contents("https://pokeapi.co/api/v2/pokemon/$id");
+        $result = json_decode($api_raw, true);
+        $abilities = array_map(
+            function ($ability) {
+                return $ability["ability"]["name"];
+            },
+            $result["abilities"]
+        );
+        $name = $result["name"];
 
-    // obtener descripción
-    $api_raw = file_get_contents("https://pokeapi.co/api/v2/pokemon-species/$id");
-    $result = json_decode($api_raw, true);
+        // obtener descripción
+        $api_raw = file_get_contents("https://pokeapi.co/api/v2/pokemon-species/$id");
+        $result = json_decode($api_raw, true);
 
-    foreach ($result["flavor_text_entries"] as $key => $value) {
-        if ($value["language"]["name"] === "es" && $value["version"]["name"] === "x") {
-            $description = $value["flavor_text"];
+        foreach ($result["flavor_text_entries"] as $key => $value) {
+            if ($value["language"]["name"] === "es" && $value["version"]["name"] === "x") {
+                $description = $value["flavor_text"];
+            }
         }
+
+        // $description = array_values(array_filter(
+        //     $result["flavor_text_entries"],
+        //     function ($value) {
+        //         return ($value["language"]["name"] === "es" && $value["version"]["name"] === "x");
+        //     }
+        // ))[0]["flavor_text"];
+
+        // $description = array_map(
+        //     function ($value) {
+        //         if ($value["language"]["name"] === "es" && $value["version"]["name"] === "x") {
+        //             return $value["flavor_text"];
+        //         }
+        //     },
+        //     $result["flavor_text_entries"]
+        // );
+
+        $pokemon->id = $id;
+        $pokemon->name = ucfirst($name);
+        $pokemon->abilities = $abilities;
+        $pokemon->description = $description;
+
+        echo json_encode($pokemon);
     }
-
-    // $description = array_values(array_filter(
-    //     $result["flavor_text_entries"],
-    //     function ($value) {
-    //         return ($value["language"]["name"] === "es" && $value["version"]["name"] === "x");
-    //     }
-    // ))[0]["flavor_text"];
-
-    // $description = array_map(
-    //     function ($value) {
-    //         if ($value["language"]["name"] === "es" && $value["version"]["name"] === "x") {
-    //             return $value["flavor_text"];
-    //         }
-    //     },
-    //     $result["flavor_text_entries"]
-    // );
-
-    $pokemon->id = $id;
-    $pokemon->name = ucfirst($name);
-    $pokemon->abilities = $abilities;
-    $pokemon->description = $description;
-
-    echo json_encode($pokemon);
 }
